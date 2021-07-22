1ece8b1b9a0caff1ff64cd6865470f45b74a3c3a
Vazgen Levonyan <vazgenlevonyan1898@gmail.com>
2021-07-22 11:55:15 +0400

    Adding patch3 for paching

---
+++ src/main/java/com/acapro/certificat/entity/forpatch3.java
@@ -0,0 +1,37 @@
+
+import com.acapro.certificat.enums.ApplicantStatusType;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "forpatch3")
+@Data
+public class forpatch3 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private int phoneNumber;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_type")
+    private ApplicantStatusType statusType;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", referencedColumnName = "id")
+    @JsonManagedReference
+    private Course course;
+}
